4cccdf0d6c37553d4cf418cbfd52100a4aec63dd
Sweetie <zsy1241920594@gmail.com>
2023-06-29 23:26:34 +0800

    update the task assignment when paimon reads

--- paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/source/ContinuousFileSplitEnumerator.java
+++ paimon-flink/paimon-flink-common/src/main/java/org/apache/paimon/flink/source/ContinuousFileSplitEnumerator.java
@@ -18,6 +18,8 @@
 
 package org.apache.paimon.flink.source;
 
+import org.apache.paimon.data.BinaryRow;
+import org.apache.paimon.flink.sink.ChannelComputer;
 import org.apache.paimon.flink.source.assigners.FIFOSplitAssigner;
 import org.apache.paimon.flink.source.assigners.PreAssignSplitAssigner;
 import org.apache.paimon.flink.source.assigners.SplitAssigner;
@@ -101,7 +103,7 @@ public class ContinuousFileSplitEnumerator
     }
 
     private void addSplit(FileStoreSourceSplit split) {
-        splitAssigner.addSplit(assignTask(((DataSplit) split.split()).bucket()), split);
+        splitAssigner.addSplit(assignTask(((DataSplit) split.split())), split);
     }
 
     @Override
@@ -206,14 +208,17 @@ public class ContinuousFileSplitEnumerator
         return assignment;
     }
 
-    private int assignTask(int bucket) {
+    private int assignTask(DataSplit dataSplit) {
         if (bucketMode == BucketMode.UNAWARE) {
             // we just assign task 0 when bucket unaware
             return 0;
         } else {
-            // if not bucket unaware, we assign the bucket % parallelism, the same bucket data go
-            // into the same task
-            return bucket % context.currentParallelism();
+            // if not bucket unaware, we assign the (partition + bucket) % parallelism,
+            // We should assign tasks of the same partition and the same bucket to the same task,
+            // and randomly assign different partitions to different tasks to make full use of parallel resources
+            BinaryRow partition = dataSplit.partition();
+            int bucket = dataSplit.bucket();
+            return ChannelComputer.select(partition,bucket,context.currentParallelism());
         }
     }
 }
